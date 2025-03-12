fb5ee6f8b715d3e5b0ce9c6f9652ed16d3c582e5
Jask <jeongys777@gmail.com>
2025-03-12 15:19:00 +0900

    run 메소드 추가

--- src/javaproject/GameMaster.java
+++ src/javaproject/GameMaster.java
@@ -300,6 +300,27 @@ class GameMaster {
 
         System.out.print(symbol);
     }
+    
+    public void run() {
+		Scanner scanner = new Scanner(System.in);
+
+		System.out.println("=== 보드게임 테스트 시작 ===");
+		printBoard();
+
+		while (!checkGoal()) {
+			System.out.println("\n엔터를 누르면 주사위를 굴립니다...");
+			scanner.nextLine();
+
+			int dice = diceRoll();
+			System.out.println("주사위: " + dice);
+
+			userMove(dice);
+			printBoard();
+		}
+
+		System.out.println("\n=== 게임 클리어! ===");
+		scanner.close();
+    }
 
 
     // getter/setter 메서드들
--- src/javaproject/GameTest.java
+++ src/javaproject/GameTest.java
@@ -4,26 +4,8 @@ import java.util.Scanner;
 
 public class GameTest {
     public static void main(String[] args) {
-        GameMaster game = new GameMaster();
-        Scanner scanner = new Scanner(System.in);
 
-        System.out.println("=== 보드게임 테스트 시작 ===");
-        game.printBoard();
-
-        
-        while (!game.checkGoal()) {
-            System.out.println("\n엔터를 누르면 주사위를 굴립니다...");
-            scanner.nextLine();
-
-            int dice = game.diceRoll();
-            
-            System.out.println("주사위: " + dice);
-
-            game.userMove(dice);
-            game.printBoard();
-        }
-
-        System.out.println("\n=== 게임 클리어! ===");
-        scanner.close();
+    	GameMaster game = new GameMaster();
+    	game.run();
     }
 }
